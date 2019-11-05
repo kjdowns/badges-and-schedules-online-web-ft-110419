06c6bbca1cbd2d602c8d2cd25be58cbe1bf59f6f
Kevin Downs <kevin.downs04@gmail.com>
2019-11-05 18:39:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,16 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(name_array)
+  badge_msg = []
+  name_array.each do |name|
+    badge_msg << badge_maker(name)
+  end
+  badge_msg
+end
+
+def assign_rooms()
+end
+
+def printer()
+end
